5d6eafef37897be46cb990cbdfb376ca8ae70610
Arthur Luiz da Silva <arthur.luiz11@hotmail.com>
2024-11-21 14:49:15 -0300

    primeiros commits

---
+++ Projeto Gerar senha/script.js
@@ -0,0 +1,15 @@
+document.getElementById('gerar').addEventListener('click', function() {
+    let comprimento = document.getElementById('comprimento').value;
+    let senha = gerarSenha(comprimento);
+    document.getElementById('senha').textContent = senha;
+});
+
+function gerarSenha(comprimento) {
+    let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let senha = '';
+    for (let i = 0; i < comprimento; i++) {
+    let indice = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres[indice];
+ }
+ return senha;
+}
